e52dd541a86dbf274159ea238f23f32d2b7cbffd
liujing <wb.liujing23@mesg.corp.netease.com>
2026-04-03 16:11:34 +0800

    cleanup: remove temp scripts

--- fix4.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-c = open(r'C:\Users\wb.liujing23\Desktop\talent-management-web\templates\index.html', encoding='utf-8').read()
-
-# Check admin section
-idx = c.find('管理员视图')
-if idx > 0:
-    chunk = c[idx:idx+600]
-    print('Admin section chunk:')
-    print(repr(chunk))
